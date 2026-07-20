f6170532485d0fe47dbf5c761d2fc528214fb09e
kellywizonbagonza379-lgtm <kellywizonbagonza379@gmail.com>
2026-07-21 01:43:50 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,51 @@
 // firebase.js
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD6AK1vue6E9P9gqLr2xRw2ZxodY3am84M  
-");  
-  authDomain: "TON_PROJET.firebaseapp.com",
-  projectId: "TON_PROJET",
-  storageBucket: "TON_PROJET.appspot.com",
-  messagingSenderId: "TON_SENDER_ID",
-  appId: "TON_APP_ID"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyD6AK1vue6E9P9gqLr2xRw2ZxodY3am84M",
+    authDomain: "white-solar-energy.firebaseapp.com",
+    projectId: "white-solar-energy",
+    storageBucket: "white-solar-energy.firebasestorage.app",
+    messagingSenderId: "492509652443",
+    appId: "1:492509652443:web:b76eb2f5c3df51151573fe",
+    measurementId: "G-7J2S41C3CG"
+  };
 
+  // Initialize Firebase
+  
+  // Initialiser Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// Authentification
+import {
+  getAuth,
+  RecaptchaVerifier,
+  signInWithPhoneNumber
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const auth = getAuth(app);
+
+// Configuration du reCAPTCHA
+window.recaptchaVerifier = new RecaptchaVerifier(auth, "recaptcha-container", {
+  size: "normal",
+});
+
+// Fonction pour envoyer le code SMS
+window.sendCode = function () {
+  const phoneNumber = document.getElementById("phone").value;
+
+  signInWithPhoneNumber(auth, phoneNumber, window.recaptchaVerifier)
+    .then((confirmationResult) => {
+      window.confirmationResult = confirmationResult;
+      alert("Code SMS envoyé !");
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
