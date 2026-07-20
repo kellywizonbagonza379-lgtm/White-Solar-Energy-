111b03142e68cdd6a91fae8a29038e9a7b1fdd32
kellywizonbagonza379-lgtm <kellywizonbagonza379@gmail.com>
2026-07-21 01:52:11 +0300

    Update login.js

--- login.js
+++ login.js
@@ -10,3 +10,32 @@ window.recaptchaVerifier = new RecaptchaVerifier(auth, "recaptcha-container", {
     console.log("reCAPTCHA validé");
   }
 });
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const phoneNumber = document.getElementById("phone").value;
+
+    const appVerifier = window.recaptchaVerifier;
+
+    try {
+        const confirmationResult = await signInWithPhoneNumber(
+            auth,
+            phoneNumber,
+            appVerifier
+        );
+
+        window.confirmationResult = confirmationResult;
+
+        const code = prompt("Entrez le code SMS reçu :");
+
+        await confirmationResult.confirm(code);
+
+        alert("Connexion réussie !");
+        window.location.href = "dashboard.html";
+
+    } catch (error) {
+        alert(error.message);
+    }
+});
